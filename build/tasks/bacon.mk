AOSPA/android_vendor_pa

tasks: bacon: Don't create identical otapackages
Saves space

Change-Id: I9b0472c617d695330614baf8b41afb918cac0dac
Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

build/tasks/bacon.mk
@@ -22,6 +22,6 @@ PA_TARGET_PACKAGE := $(PRODUCT_OUT)/pa$(PA_VERSION).zip
 
 .PHONY: bacon
 bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
-	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(PA_TARGET_PACKAGE)
+	$(hide) mv $(INTERNAL_OTA_PACKAGE_TARGET) $(PA_TARGET_PACKAGE)
 	$(hide) $(MD5SUM) $(PA_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(PA_TARGET_PACKAGE).md5sum
 	@echo "Package Complete: $(PA_TARGET_PACKAGE)" >&2

Wave-Project/vendor_wave

build/tasks/bacon.mk
@@ -25,7 +25,7 @@ WAVE_TARGET_PACKAGE_FOLDER := $(PRODUCT_OUT)
 
 .PHONY: bacon
 bacon: $(INTERNAL_OTA_PACKAGE_TARGET)
-	$(hide) ln -f $(INTERNAL_OTA_PACKAGE_TARGET) $(WAVE_TARGET_PACKAGE)
+	$(hide) mv $(INTERNAL_OTA_PACKAGE_TARGET) $(WAVE_TARGET_PACKAGE)
 	$(hide) $(MD5SUM) $(WAVE_TARGET_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(WAVE_TARGET_PACKAGE).md5sum
 	@echo -e ""
 	@echo -e ${CL_CYN}"==========================================================================="${CL_RST}