2b88f4fde85f72cccc5438c4c2a8b361fd616f64
Jake Weinstein <jake@aospa.co>
2019-09-26 13:09:03 +0530

    vendor: enable Snapdragon LLVM 8.0 Compiler
    passes CTS and has no stability issues.
    
    also reverts ''''
    sdclang: Add Pie configuration (a3fb97e3af54dd704ad1c560600860390e22eb53)
    ''''
    
    Change-Id: I6d46ef2c208fecb8c8032c5ceb3bec09bc6e74b4

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -8,7 +8,6 @@ $(call add_json_bool, Has_legacy_camera_hal1, 				$(filter true,$(TARGET_HAS_LEG
 $(call add_json_str_omitempty, Target_process_sdk_version_override,	$(TARGET_PROCESS_SDK_VERSION_OVERRIDE))
 $(call add_json_str_omitempty, Target_shim_libs,     			$(subst $(space),:,$(TARGET_LD_SHIM_LIBS)))
 $(call add_json_bool, Target_uses_color_metadata,			$(filter true,$(TARGET_USES_COLOR_METADATA)))
-$(call add_json_bool, Target_use_sdclang,				$(filter true,$(if $(strip $(TARGET_USE_SDCLANG)),true,false)))
 $(call add_json_bool, Uses_qti_camera_device,				$(filter true,$(TARGET_USES_QTI_CAMERA_DEVICE)))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,3 +3,8 @@ caf_devices=('rolex' 'oneplus3' 'A6020')
 for device in ${caf_devices[@]}; do
     add_lunch_combo wave_$device-userdebug
 done
+
+# SDClang Environment Variables
+export SDCLANG_AE_CONFIG=$(pwd)/vendor/wave/sdclang/sdclangAE.json
+export SDCLANG_CONFIG=$(pwd)/vendor/wave/sdclang/sdclang.json
+export SDCLANG_SA_ENABLED=false
